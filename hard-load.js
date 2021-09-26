77517f763dbec486185ccc8ede00f0b67381e572
Lucian Caetano <lucian.santos.caetano@gmail.com>
2021-09-26 01:49:28 -0300

    fix(types): :pencil2:  add types to package

--- hard-load.js
+++ hard-load.js
@@ -8,20 +8,24 @@ const resolveDbName = (name) => path.join(__dirname, 'db', name);
 
     await blockchain.createGenesisBlock();
 
-    // add 700000 blocks
-    for (let i = 0; i < 700000; i+=100) {
+    // add 1000 blocks
+    for (let i = 0; i < 1000; i += 100) {
         const promises = [];
-        for (let j = 0; j < 10000; j++) {
-            promises.push(blockchain.addBlock(`block ${i}-${j}`));
+        for (let j = 0; j < 100; j++) {
+            promises.push(blockchain.addBlock({ data: `block ${i}-${j}` }));
         }
 
         await Promise.all(promises);
 
         // print progress percent
-        const percent = ((i / 700000) * 100).toFixed(2);
-        process.stdout.write(`\r${percent}% ${i}/${700000}`);
+        const percent = ((i / 1000) * 100).toFixed(2);
+        process.stdout.write(`\r${percent}% ${i}/${1000}`);
     }
 
     const lastBlock = await blockchain.getLastBlock();
+    const valid = blockchain.validate();
+
+    console.log(`\n\n${valid ? 'valid' : 'invalid'} blockchain`);
+
     console.info(lastBlock);
 })();
